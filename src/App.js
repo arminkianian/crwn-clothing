e6db812bfdb8dfbbdef3e48cdf6b21ff9ba27aeb
Armin Kianian <arminkia.dev@gmail.com>
2019-09-09 13:12:58 +0430

    update out menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 
+const HatsPage = props => {
+  console.log(props);
+  return <h1>Hats Page</h1>;
+};
+
 function App() {
-  return <HomePage></HomePage>;
+  return (
+    <Switch>
+      <Route exact path="/" component={HomePage}></Route>
+      <Route path="/shop/hats" component={HatsPage}></Route>
+    </Switch>
+  );
 }
 
 export default App;
