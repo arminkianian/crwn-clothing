3e72aca37eff56513940488f79ca9a68bfd136d2
Armin Kianian <arminkia.dev@gmail.com>
2019-09-10 22:52:09 +0430

    creating signin page and sign in components, form-input, custom-butto…

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 const HatsPage = props => {
   console.log(props);
@@ -17,6 +18,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
         <Route path="/shop" component={ShopPage}></Route>
+        <Route path="/signin" component={SignInAndSignUp}></Route>
       </Switch>
     </div>
   );
